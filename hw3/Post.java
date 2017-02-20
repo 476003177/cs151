42f156690bae606ac166c67e6bd2adf4b0240d5e
Tyler Wasniowski <TylerWasniowski@gmail.com>
2017-02-20 01:07:29 -0800

    Commented code with javadocs

--- hw3/Post.java
+++ hw3/Post.java
@@ -3,22 +3,44 @@
  */
 public class Post {
 
+    /**
+     * author
+     */
     private User author;
+    /**
+     * last updater
+     */
     private User lastUpdater;
+    /**
+     * text
+     */
     private String text;
 
+    /**
+     * Post constructor
+     */
     public Post() {
         this.author = null;
         this.lastUpdater = null;
         this.text = null;
     }
 
+    /**
+     * Post constructor with author and text
+     * @param author Person who wrote the post
+     * @param text Text of post
+     */
     public Post(User author, String text) {
         this.author = author;
         this.lastUpdater = author;
         this.text = text;
     }
 
+    /**
+     * Updates Post
+     * @param user new updater
+     * @param text post text
+     */
     public void update(User user, String text) {
         if (this.author == null)
             author = user;
@@ -26,14 +48,26 @@ public class Post {
         this.text = text;
     }
 
+    /**
+     * gets author
+     * @return author
+     */
     public User getAuthor() {
         return author;
     }
 
+    /**
+     * gets last updater
+     * @return the user who last updated this post
+     */
     public User getLastAuthor() {
         return lastUpdater;
     }
 
+    /**
+     * gets text of post
+     * @return text of post
+     */
     public String getText() {
         return text;
     }
