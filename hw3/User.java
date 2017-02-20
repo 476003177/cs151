29a6415aca32d8407d5721cb8722ddee59043efe
Tyler Wasniowski <TylerWasniowski@gmail.com>
2017-02-19 22:19:14 -0800

    Finished part of User

--- hw3/User.java
+++ hw3/User.java
@@ -2,4 +2,26 @@
  * Created by Tyler on 2/15/2017.
  */
 public class User {
+    public enum Type {INSTRUCTOR, STUDENT}
+
+    private String username;
+    private String password;
+    private Type role;
+
+    public User(String username, String password, Type role) {
+        this.username = username;
+        this.password = password;
+
+        this.role = role;
+    }
+
+    public boolean authenticate(String username, String password) {
+        return this.username.equals(username) && this.password.equals(password);
+    }
+
+    public boolean isInstructor() {
+        return role.equals(Type.INSTRUCTOR);
+    }
+
+
 }
