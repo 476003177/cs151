c59a1de1bb6fa9e445e8ea2769efae91be38e0f9
Tyler Wasniowski <Tyler Wasniowski>
2017-03-01 14:47:55 -0800

    End of class, lecture 10

--- lab10/AnimationTester.java
+++ lab10/AnimationTester.java
@@ -1,5 +1,6 @@
 import java.awt.*;
-import java.awt.event.*;
+import java.util.HashSet;
+import java.util.Random;
 import javax.swing.*;
 
 /**
@@ -12,31 +13,86 @@ public class AnimationTester
    {
       JFrame frame = new JFrame();
 
-      final MoveableShape shape
-            = new CarShape(0, 0, CAR_WIDTH);
+      HashSet<Car> cars = new HashSet<>();
+      HashSet<Ball> balls = new HashSet<>();
 
-      ShapeIcon icon = new ShapeIcon(shape,
-            ICON_WIDTH, ICON_HEIGHT);
+      for (int n = 0; n < 0; n++)
+         cars.add(new Car());
+      for (int n = 0; n < 1; n++)
+         balls.add(new Ball());
 
-      final JLabel label = new JLabel(icon);
       frame.setLayout(new FlowLayout());
-      frame.add(label);
+      for (Car c: cars)
+         frame.add(c.getLabel());
+      for (Ball b: balls)
+         frame.add(b.getLabel());
 
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       frame.pack();
+      frame.setMinimumSize(new Dimension(300, 600));
       frame.setVisible(true);
 
       final int DELAY = 100;
       // Milliseconds between timer ticks
       Timer t = new Timer(DELAY, event ->
          {
-            shape.move();
-            label.repaint();
+            for (Car c: cars)
+               c.update();
+            for (Ball b: balls)
+               b.update();
          });
       t.start();
    }
 
-   private static final int ICON_WIDTH = 400;
-   private static final int ICON_HEIGHT = 100;
-   private static final int CAR_WIDTH = 100;
+   private static class Car {
+      MoveableShape shape;
+      ShapeIcon icon;
+      JLabel label;
+
+      public Car() {
+         shape = new CarShape(0, 0, CAR_WIDTH);
+         icon = new ShapeIcon(shape, ICON_WIDTH, ICON_HEIGHT);
+         label = new JLabel(icon);
+      }
+
+      public void update() {
+            shape.move();
+            label.repaint();
+      }
+
+      public JLabel getLabel() {
+         return label;
+      }
+
+      private static final int ICON_WIDTH = 400;
+      private static final int ICON_HEIGHT = 100;
+      private static final int CAR_WIDTH = 100;
+
+   }
+
+   private static class Ball {
+      MoveableShape shape;
+      ShapeIcon icon;
+      JLabel label;
+
+      public Ball() {
+         shape = new BouncingBall(BALL_WIDTH);
+         icon = new ShapeIcon(shape, ICON_WIDTH, ICON_HEIGHT);
+         label = new JLabel(icon);
+      }
+
+      public void update() {
+         shape.move();
+         label.repaint();
+      }
+
+      public JLabel getLabel() {
+         return label;
+      }
+
+      private static final int ICON_WIDTH = 100;
+      private static final int ICON_HEIGHT = 400;
+      private static final int BALL_WIDTH = 20;
+   }
+
 }
